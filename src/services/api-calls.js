9854491537e03876910b5307ab881cd04aaaa0ef
reiosantos <ronireiosantos@gmail.com>
2018-09-16 08:07:31 +0300

    Merge remote-tracking branch 'origin/landing' into develop
    # Conflicts:
    #	src/App.css
    #	src/App.js

--- src/services/api-calls.js
+++ src/services/api-calls.js
@@ -4,12 +4,12 @@ var citesLegislation = (animal_id) => {
     var headers = {
       'Content-Type': 'application/json',
       'X-Authentication-Token': 'Ke8QltCkeS3LRyvav4FrrQtt'
-    }
-    animal_id = 5924;
+    };
+    animal_id = 4521;
     return axios.get(process.env.REACT_APP_API_URL+'/api/v1/taxon_concepts/'+animal_id+'/cites_legislation', {headers: headers})
     .then((res) => {
       console.log(res.data)
     })
-  }
+  };
 
   export default citesLegislation;
